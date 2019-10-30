b47624a817adabf0770790db9287f39a3628154f
stormkirill98 <stormkirill98@gmail.com>
2019-10-30 22:05:27 +0300

    return events from database

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-
 client = MongoClient("mongodb+srv://admin:admin@socialcalendar-kjwvs.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
 
 
 @app.route('/')
 def run():
-    return "qu qu"
+    events = ""
+    for doc in client['database']['events'].find({}):
+        events += doc + "\n"
+    return events
