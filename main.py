6f98ba13a321d6219873cb26cb4ea1f5c4667a4d
stormkirill98 <stormkirill98@gmail.com>
2019-10-30 22:01:03 +0300

    connect mongodb

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+
+client = MongoClient("mongodb+srv://admin:admin@socialcalendar-kjwvs.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
 
 
 @app.route('/')
