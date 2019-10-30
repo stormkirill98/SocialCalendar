7155cc2d27ca262e098a081bba25a6cce2324211
SmirnovAr <56647933+SmirnovAr@users.noreply.github.com>
2019-10-30 23:06:29 +0300

    birth_date upd to birthdate

--- shared/user.py
+++ shared/user.py
@@ -4,11 +4,11 @@ class User:
     friend_id_list = {}
     chat_id_list = {}
 
-    def __init__(self, id, login, password, nickname, avatar_url, birth_date):
+    def __init__(self, id, login, password, nickname, avatar_url, birthdate):
         """Constructor"""
         self.id = id
         self.login = login
         self.password = password
         self.nickname = nickname
         self.avatar_url = avatar_url
-        self.birth_date = birth_date
+        self.birthdate = birthdate
