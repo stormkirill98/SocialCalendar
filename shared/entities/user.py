69849859db9cd3d05844907273c94820ffa5afa0
stormkirill98 <stormkirill98@gmail.com>
2019-11-01 22:33:14 +0300

    write functions for add event, friend, chat to user

--- shared/entities/user.py
+++ shared/entities/user.py
@@ -1,7 +1,7 @@
 class User:
-    event_id_list = {}
-    friend_id_list = {}
-    chat_id_list = {}
+    event_id_list = []
+    friend_id_list = []
+    chat_id_list = []
 
     def __init__(self, login, password, nickname, avatar_url, birthday, id=""):
         """Constructor"""
@@ -14,6 +14,15 @@ class User:
 
     def set_id(self, id):
         self.id = id
+
+    def add_event(self, id):
+        self.event_id_list.__add__(id)
+
+    def add_friend(self, id):
+        self.friend_id_list.__add__(id)
+
+    def add_chat(self, id):
+        self.chat_id_list.__add__(id)
 
     def to_json(self):
         return {
