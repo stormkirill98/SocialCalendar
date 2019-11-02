95b40d626f8cbf0812a398f280d601daaadc50d5
stormkirill98 <stormkirill98@gmail.com>
2019-11-02 17:53:31 +0300

    rename private chat on dialog

--- server/entities/chats/dialog.py
+++ server/entities/chats/dialog.py
@@ -1,7 +1,7 @@
 from server.entities.chats.chat import Chat
 
 
-class PrivateChat(Chat):
+class Dialog(Chat):
     def __init__(self, user_id_1, user_id_2, id="", msg_id_list=[]):
         super().__init__(id, msg_id_list)
         self.user_id_1 = user_id_1
