37799c91b1c86ecf257bdd438791de9d7dfc5705
stormkirill98 <stormkirill98@gmail.com>
2019-11-02 23:17:29 +0300

    delete dialog

--- server/utils.py
+++ server/utils.py
@@ -3,13 +3,36 @@ from server.entities.chats.dialog import Dialog
 
 
 def create_dialog(user_id_1, user_id_2):
+    """Create and save dialog to mongoDB,
+       then add this dialog to users on mongoDB"""
+
+    # TODO check that users id is validate and users already is created
+
+    # create and save dialog
     dialog = Dialog(user_id_1, user_id_2)
     dialog = chat_dao.save_dialog(dialog)
 
+    # add chat to users
     user_dao.add_chat(user_id_1, dialog.id)
     user_dao.add_chat(user_id_2, dialog.id)
 
     return dialog
 
 
+def delete_dialog(dialog_id):
+    """Delete dialog, remove it in users, remove all msg's in it"""
 
+    # get dialog by id
+    dialog = chat_dao.get_dialog(dialog_id)
+
+    # TODO check that dialog is not null
+    # delete dialog from user
+    user_dao.delete_chat(dialog.user_id_1, dialog_id)
+    user_dao.delete_chat(dialog.user_id_2, dialog_id)
+
+    # delete msg's
+    for msg_id in dialog.msg_id_list:
+        chat_dao.delete_msg(msg_id)
+
+    # delete dialog
+    chat_dao.delete_dialog(dialog_id)
