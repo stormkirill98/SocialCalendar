e207208d90640019ab95b3d201f6a2d03c2b7031
stormkirill98 <stormkirill98@gmail.com>
2019-11-04 11:41:48 +0300

    create invite_dao

---
+++ server/datastore/invite_dao.py
@@ -0,0 +1,27 @@
+from bson import ObjectId
+
+from server.datastore.datastore import database
+from server.entities.invite import Invite
+
+invites_collection = database['invites']
+
+
+def save_invite(invite):
+    json = invite.to_json()
+    json.pop('id')
+
+    id = invites_collection.insert_one(json).inserted_id
+    invite.set_id(id)
+    return invite
+
+
+def get_invite(id):
+    json = invites_collection.find_one({'_id': ObjectId(id)})
+    return Invite(json['sender_id'],
+                  json['type'],
+                  json['place_id'],
+                  str(json['_id']))
+
+
+def delete_invite(id):
+    return invites_collection.delete_one({'_id': ObjectId(id)}).deleted_count
--- server/entities/invite.py
+++ server/entities/invite.py
@@ -2,16 +2,16 @@ from server.entities.datastore_object import DatastoreObject
 
 
 class Invite(DatastoreObject):
-    def __init__(self, sender_id, invite_type, place_id, id=""):
+    def __init__(self, sender_id, type, place_id, id=""):
         super().__init__(id)
         self.sender_id = sender_id
-        self.invite_type = invite_type
+        self.type = type
         self.place_id = place_id  # ид куда приглашают
 
     def to_json(self):
         return {
             'id': self.id,
             'sender_id': self.sender_id,
-            'invite_type': self.invite_type,
+            'type': self.type,
             'place_id': self.place_id
         }
