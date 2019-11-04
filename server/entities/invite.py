c2a98cd702c793eb352417327cf255d1ea0739ad
stormkirill98 <stormkirill98@gmail.com>
2019-11-04 11:31:39 +0300

    add to_json() to Invite

--- server/entities/invite.py
+++ server/entities/invite.py
@@ -6,4 +6,12 @@ class Invite(DatastoreObject):
         super().__init__(id)
         self.sender_id = sender_id
         self.invite_type = invite_type
-        self.place_id = place_id
+        self.place_id = place_id  # ид куда приглашают
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'sender_id': self.sender_id,
+            'invite_type': self.invite_type,
+            'place_id': self.place_id
+        }
