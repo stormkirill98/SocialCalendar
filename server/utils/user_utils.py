6ed00d96a0493503487b5a8ebf59e725bae70e07
stormkirill98 <stormkirill98@gmail.com>
2019-11-05 22:44:13 +0300

    create method send_msg

--- server/utils/user_utils.py
+++ server/utils/user_utils.py
@@ -1,8 +1,12 @@
-from server.datastore import invite_dao, user_dao
-from server.enums import InviteType
+from datetime import datetime
+
+from server.datastore import invite_dao, user_dao, msg_dao, chat_dao
+from server.entities.chats.inner_classes.message import Message
+from server.enums import InviteType, ChatType
 from server.utils.events import group_event_utils
 
 
+# not test
 def accept_invite(user_id, invite_id):
     invite = invite_dao.get_invite(invite_id)
 
@@ -14,3 +18,13 @@ def accept_invite(user_id, invite_id):
         group_event_utils.add_member_to_group_event(user_id, invite.place_id)
 
     invite_dao.delete_invite(invite_id)
+
+
+# not test
+def send_msg(user_id, chat_id, chat_type, msg_text):
+    msg = Message(user_id, chat_id, datetime.today(), msg_text)
+    msg_id = msg_dao.save_msg(msg)
+    if chat_type == ChatType.DIALOG:
+        chat_dao.add_msg_to_dialog(chat_id, msg_id)
+    else:
+        chat_dao.add_msg_to_event_chat(chat_id, msg_id)
