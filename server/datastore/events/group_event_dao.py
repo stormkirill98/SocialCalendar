1162996c1f1b215acd002d18b34153353c38857f
stormkirill98 <stormkirill98@gmail.com>
2019-11-05 22:27:37 +0300

    save, delete, get group_event, add member to group event

---
+++ server/datastore/events/group_event_dao.py
@@ -0,0 +1,36 @@
+from bson import ObjectId
+
+from server.datastore.datastore import database
+from server.entities.events.group_events.group_event import GroupEvent
+
+group_event_collection = database['group_events']
+
+
+def save(group_event):
+    json = group_event.to_json()
+    json.pop('id')
+
+    id = group_event_collection.insert_one(json).inserted_id
+    group_event.set_id(id)
+    return id
+
+
+def delete(id):
+    return group_event_collection.delete_one({'_id': ObjectId(id)}).deleted_count
+
+
+def get(id):
+    json = group_event_collection.find_one({'_id': ObjectId(id)})
+    return GroupEvent(json['name'],
+                      json['is_private'],
+                      json['datetime'],
+                      json['address'],
+                      json['description'],
+                      json['member_id_list'],
+                      json['id'])
+
+
+def add_member(group_event_id, member_id):
+    result = group_event_collection.update_one({'_id': ObjectId(group_event_id)},
+                                               {'$push': {'event_id_list': ObjectId(member_id)}})
+    return result.modified_count
