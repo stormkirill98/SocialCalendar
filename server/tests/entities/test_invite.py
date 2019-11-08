a8692fe5e42ef0f1cbff8e40e4d5eba1c1fb1cab
stormkirill98 <stormkirill98@gmail.com>
2019-11-08 21:22:57 +0300

    create test for invite

---
+++ server/tests/entities/test_invite.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from server.entities.invite import Invite
+
+
+class TestInvite(TestCase):
+    def setUp(self):
+        self.invite = Invite("sender_id", "receiver_id", "invite_type", "place_id", "id")
+        
+    def test_to_json(self):
+        self.assertEqual(self.invite.to_json(), {
+            'id': 'id',
+            'sender_id': 'sender_id',
+            'receiver_id': 'receiver_id',
+            'type': 'invite_type',
+            'place_id': 'place_id'
+        })
