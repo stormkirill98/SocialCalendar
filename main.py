4be9c4f3c558e2123ad07e059a25b35bf71e99d6
stormkirill98 <stormkirill98@gmail.com>
2019-11-15 23:18:58 +0300

    Revert "Revert "set up environment vars""
    This reverts commit da445fd0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import local
 
 from flask import Flask, redirect, request, url_for
 from flask_login import (
@@ -12,13 +13,20 @@ from flask_login import (
 from oauthlib.oauth2 import WebApplicationClient
 import requests
 
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def run():
-    return "Hello world"
+    return GOOGLE_CLIENT_SECRET
 
 # uncomment for debug locale
-# app.run()
+app.run()
