4797bacc2d915e2a4af13f0e0b08e9fdb5f167ee
stormkirill98 <stormkirill98@gmail.com>
2019-11-15 19:39:47 +0300

    change text in msg

--- server/database/msg_dao.py
+++ server/database/msg_dao.py
@@ -11,34 +11,44 @@ def save_msg(msg):
     json = msg.to_json()
     json.pop('id')
 
-    id = msg_collection.insert_one(json).inserted_id
-    msg.set_id(id)
-    return id
+    msg_id = msg_collection.insert_one(json).inserted_id
+    msg.set_id(msg_id)
+    return msg_id
 
 
-def get_msg(id):
-    if not id_is_valid(id):
+def change_text(msg_id, text):
+    if not id_is_valid(msg_id):
+        return 0
+
+    result = msg_collection.update_one({'_id': ObjectId(msg_id)},
+                                       {'$set': {'text': text}})
+    return result.modified_count
+
+
+def get_msg(msg_id):
+    if not id_is_valid(msg_id):
         return None
 
-    json = msg_collection.find_one({'_id': ObjectId(id)})
+    json = msg_collection.find_one({'_id': ObjectId(msg_id)})
     if json is None:
         return None
 
     return Message(json['user_id'],
+                   json['chat_id'],
                    json['datetime'],
-                   json['test'],
+                   json['text'],
                    json['_id'])
 
 
-def delete_msg(id):
-    if not id_is_valid(id):
+def delete_msg(msg_id):
+    if not id_is_valid(msg_id):
         return 0
 
-    return msg_collection.delete_one({'_id': ObjectId(id)}).deleted_count
+    return msg_collection.delete_one({'_id': ObjectId(msg_id)}).deleted_count
 
 
-def is_exists(id):
-    if not id_is_valid(id):
+def is_exists(msg_id):
+    if not id_is_valid(msg_id):
         return False
 
-    return database.is_exist(id, msg_collection)
+    return database.is_exist(msg_id, msg_collection)
