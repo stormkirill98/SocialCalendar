a308101d48d0c501c3e97454e48e5bb80369b21b
stormkirill98 <stormkirill98@gmail.com>
2019-11-15 22:17:18 +0300

    add imports

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from flask import Flask
+import json
+import os
+
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
 
 app = Flask(__name__)
 
