8a3bf08edaa3f3da8b0dd4708b4c0397c60a9dec
stormkirill98 <stormkirill98@gmail.com>
2019-11-15 23:18:10 +0300

    set up environment vars

---
+++ local.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['GOOGLE_CLIENT_SECRET'] = 'nZKk83_UNwrCg0QECwgwOnRQ'
+os.environ['GOOGLE_CLIENT_ID'] = '1033315078324-tb38sppml93t1kpdmrm3ukgqg873meap.apps.googleusercontent.com'
