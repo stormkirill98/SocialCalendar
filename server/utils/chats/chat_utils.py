b6c92ffd73125d2ce110dc73932f7fc092936253
stormkirill98 <stormkirill98@gmail.com>
2019-11-17 19:37:18 +0300

    add doc to method

--- server/utils/chats/chat_utils.py
+++ server/utils/chats/chat_utils.py
@@ -21,6 +21,9 @@ def get_add_chats(user_id, start_num, end_num):
 
 
 def get_sorted_chats(user_id):
+    """Return all chats from user sorted by last message datetime
+    :return List<Chat>"""
+
     user = user_dao.get_user(user_id)
 
     user_chats = []
