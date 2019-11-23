ea4320c7103fb348e355edad00236e39b2183839
stormkirill98 <stormkirill98@gmail.com>
2019-11-23 15:37:11 +0300

    filter events by month

--- server/utils/events/event_utils.py
+++ server/utils/events/event_utils.py
@@ -1,3 +1,7 @@
+import calendar
+
+from datetime import datetime
+
 from bson import json_util
 from werkzeug.exceptions import abort
 
@@ -17,9 +21,17 @@ def get_events(month, year, user: User):
 
     month = int(month)
     year = int(year)
-    if month < 0 or month > 11:
-        return abort(400, {'message': 'Month must be number between 0 and 11 inclusive'})
+    if month < 1 or month > 12:
+        return abort(400, {'message': 'Month must be number between 1 and 12 inclusive'})
 
     events: list = event_dao.get_events(user.event_id_list)
+
+    # filter events which contains in require month
+    start_date = datetime(year, month, 1)
+    end_date = datetime(year,
+                        month,
+                        calendar.monthrange(year, month)[1],
+                        23, 59, 59)
+    events = [x for x in events if start_date <= x.datetime <= end_date]
 
     return json_util.dumps([e.__dict__ for e in events])
