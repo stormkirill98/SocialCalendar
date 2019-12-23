218027f8ee74ebb9a2321540b3f6f9b399afb600
Max <max.dobro@mail.ru>
2019-12-23 19:35:13 +0300

    хедер пофиксил

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,18 +4,27 @@ import Nav from "react-bootstrap/Nav";
 import "../css/Header.css"
 import Form from "react-bootstrap/Form";
 import UserCard from "./UserCard";
+import { Link } from "react-router-dom";
 
 export default class Header extends React.Component {
     render() {
         return (
             <Navbar bg="dark" variant="dark" className="navbar">
-                <Navbar.Brand href="../Calendar" className="title">Social Calendar</Navbar.Brand>
+                <Navbar.Brand href="../Calendar" className="navbar-brand">
+                    <Link className="header-link" to="../Calendar">Social Calendar</Link>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav>
-                        <Nav.Link href="../Calendar">Календарь</Nav.Link> {/*Хз href так или не так, но он робит */}
-                        <Nav.Link href="../Friends">Друзья</Nav.Link>
-                        <Nav.Link href="../Dialogues">Чаты</Nav.Link>
+                        <Nav.Link >
+                            <Link className="header-link" to="../Calendar">Календарь</Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link className="header-link" to="../Friends">Друзья</Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link className="header-link" to="../Dialogues">Чаты</Link>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
 
