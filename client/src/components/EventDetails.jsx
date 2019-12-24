4e24741f74a807e8e89db95acf32e23c4787b9bf
SmirnovAr <art322sm@yandex.ru>
2019-12-25 02:27:27 +0300

    переписал EventDetails

--- client/src/components/EventDetails.jsx
+++ client/src/components/EventDetails.jsx
@@ -1,37 +1,20 @@
 import React from "react";
 import "../css/EventDetails.css"
-// import {Link} from "react-router-dom";
 
-export default class EventMember extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            id: props.id,
-
-        };
-
-        fetch(`https://social-calendar-tensor.herokuapp.com/friend?id=${this.props.id}}`).then((response) => {
-            if (response.ok) {
-                response.json().then((data) => {
-                    this.state.name = data.name
-                    this.state.avatar = data.profile_pic
-                })
-            } else {
-                console.log(response.statusText);
-            }
-        });
-    }
-
+export default class EventDetails extends React.Component {
+    
     render() {
         return (
-                <div className="event-member">
-                    <img className="event-member-ava" src={this.state.avatar} alt="Аватарка" width="20%" />
-                    <div className="event-member-name">
-                        {this.state.name}
-                    </div>
+            <>
+            <div className="event-details">
+                <img className="detail-icon" src={this.props.icon} alt="icon"/>
+                <div className="wrap1">
+                    <div className="detail-title">{this.props.title}</div>
+                    <div className="detail-description">{this.props.description} </div>
                 </div>
+            </div>
+            </>
         );
     }
 }
