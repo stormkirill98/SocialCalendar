e444f0e7ef67f027aeadb56004436814a78a3585
SmirnovAr <art322sm@yandex.ru>
2020-01-13 18:37:57 +0300

    build

---
+++ client/build/precache-manifest.4d1aa31ea8e7c95d37f71782aae623fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba64e4b272c012131b06d150c4e24f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "256e4940aa74c6919d60",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "196ab58d18aff46e6138",
+    "url": "/static/css/main.df18a28f.chunk.css"
+  },
+  {
+    "revision": "256e4940aa74c6919d60",
+    "url": "/static/js/2.053c427c.chunk.js"
+  },
+  {
+    "revision": "196ab58d18aff46e6138",
+    "url": "/static/js/main.2e7bb3b0.chunk.js"
+  },
+  {
+    "revision": "92158908df98c2ae5a28",
+    "url": "/static/js/runtime-main.47e08d05.js"
+  },
+  {
+    "revision": "b60bb3c69fd2ba6576a27f876ea95042",
+    "url": "/static/media/Algerian.b60bb3c6.TTF"
+  },
+  {
+    "revision": "d81bcfdcf6a12564f56b311c69efa8ed",
+    "url": "/static/media/Auth-background.d81bcfdc.jpg"
+  },
+  {
+    "revision": "c6e75c47abf28e948518abf8d2fd7975",
+    "url": "/static/media/w512h5121371227427events.c6e75c47.png"
+  }
+]);
