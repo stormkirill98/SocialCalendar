a56c67478760b8995493c7fc65b386ae78c715ea
stormkirill98 <stormkirill98@gmail.com>
2020-01-17 13:37:46 +0300

    rebuild

---
+++ client/build/precache-manifest.433933e35c6ccf12ca0d490f254e5ccc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a66d38e51755505f0416d2a29775d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c929b8501a11688b829e",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7f24b3165fbcf66ec560",
+    "url": "/static/css/main.5a7b28ce.chunk.css"
+  },
+  {
+    "revision": "c929b8501a11688b829e",
+    "url": "/static/js/2.4a303a7c.chunk.js"
+  },
+  {
+    "revision": "7f24b3165fbcf66ec560",
+    "url": "/static/js/main.685cd74f.chunk.js"
+  },
+  {
+    "revision": "92158908df98c2ae5a28",
+    "url": "/static/js/runtime-main.47e08d05.js"
+  },
+  {
+    "revision": "b60bb3c69fd2ba6576a27f876ea95042",
+    "url": "/static/media/Algerian.b60bb3c6.TTF"
+  },
+  {
+    "revision": "d81bcfdcf6a12564f56b311c69efa8ed",
+    "url": "/static/media/Auth-background.d81bcfdc.jpg"
+  },
+  {
+    "revision": "c6e75c47abf28e948518abf8d2fd7975",
+    "url": "/static/media/w512h5121371227427events.c6e75c47.png"
+  }
+]);
